423a720aeddc67b38fd92ea83233aabf9889e57e
Arialdo Martini <arialdomartini@gmail.com>
2016-03-18 19:35:13 +0100

    haproxy balances 5672 port as well

--- pubsub/Producer/Program.cs
+++ pubsub/Producer/Program.cs
@@ -1,12 +1,45 @@
 ﻿using System;
+using RabbitMQ.Client;
+using System.Text;
 
 namespace Producer
 {
-    class MainClass
+    class Producer
     {
         public static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var loadBalancerIp = args[0];
+            var queueName = "clustertest";
+            var factory = new ConnectionFactory() { HostName = loadBalancerIp };
+            using(var connection = factory.CreateConnection())
+            using(var channel = connection.CreateModel())
+            {
+                channel.QueueDeclare(queue: queueName,
+                    durable: true,
+                    exclusive: false,
+                    autoDelete: false,
+                    arguments: null);
+
+                var i = 0;
+                while(++i>0)
+                {
+                    var message = string.Format("Message {0}", i);
+                    var body = Encoding.UTF8.GetBytes(message);
+
+                    var properties = channel.CreateBasicProperties();
+                    properties.SetPersistent(true);
+
+                    channel.BasicPublish(exchange: "",
+                        routingKey: queueName,
+                        basicProperties: properties,
+                        body: body);
+                    Console.WriteLine(" [x] Sent {0}", message);
+                }
+                    
+            }
+
+            Console.WriteLine(" Press [enter] to exit.");
+            Console.ReadLine();
         }
     }
 }

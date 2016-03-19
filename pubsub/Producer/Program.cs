ccc6927cf19090c750204f9c2ee3f009b7070651
Arialdo Martini <arialdomartini@gmail.com>
2016-03-19 07:03:13 +0100

    A producer not able to auto-reconnect

--- pubsub/Producer/Program.cs
+++ pubsub/Producer/Program.cs
@@ -1,45 +1,79 @@
 ﻿using System;
 using RabbitMQ.Client;
 using System.Text;
+using System.Threading;
 
 namespace Producer
 {
-    class Producer
+    class Program
     {
         public static void Main(string[] args)
         {
             var loadBalancerIp = args[0];
-            var queueName = "clustertest";
-            var factory = new ConnectionFactory() { HostName = loadBalancerIp };
-            using(var connection = factory.CreateConnection())
-            using(var channel = connection.CreateModel())
+
+            var producer = new Producer(new ChannelFactory(loadBalancerIp));
+            producer.Run();
+        }
+    }
+
+    class ChannelFactory
+    {
+        readonly string _serverIp;
+        readonly ConnectionFactory _connectioFactory;
+        IConnection _connection;
+        IModel _channel;
+
+        public ChannelFactory(string serverIp)
+        {
+            _serverIp = serverIp;
+            _connectioFactory = new ConnectionFactory() { HostName = _serverIp };
+
+        }
+
+        public IModel GetChannel()
+        {
+            _connection = _connectioFactory.CreateConnection();
+            _channel = _connection.CreateModel();
+            return _channel;
+        }
+    }
+
+    class Producer
+    {
+        readonly string queueName = "clustertest";
+        ChannelFactory _channelFactory;
+
+        public Producer(ChannelFactory channelFactory)
+        {
+            this._channelFactory = channelFactory;
+        }
+
+        IModel Setup()
+        {
+            var channel = _channelFactory.GetChannel();
+            channel.QueueDeclare(queue: queueName, durable: true, exclusive: false, autoDelete: false, arguments: null);
+            return channel;
+        }
+
+        void PublishMessages(IModel channel)
+        {
+            var i = 0;
+            while (++i > 0)
             {
-                channel.QueueDeclare(queue: queueName,
-                    durable: true,
-                    exclusive: false,
-                    autoDelete: false,
-                    arguments: null);
-
-                var i = 0;
-                while(++i>0)
-                {
-                    var message = string.Format("Message {0}", i);
-                    var body = Encoding.UTF8.GetBytes(message);
-
-                    var properties = channel.CreateBasicProperties();
-                    properties.SetPersistent(true);
-
-                    channel.BasicPublish(exchange: "",
-                        routingKey: queueName,
-                        basicProperties: properties,
-                        body: body);
-                    Console.WriteLine(" [x] Sent {0}", message);
-                }
-                    
+                var message = string.Format("Message {0}", i);
+                var body = Encoding.UTF8.GetBytes(message);
+                var properties = channel.CreateBasicProperties();
+                properties.SetPersistent(true);
+                channel.BasicPublish(exchange: "", routingKey: queueName, basicProperties: properties, body: body);
+                Console.WriteLine(" [x] Sent {0}", message);
+                Thread.Sleep(300);
             }
+        }
 
-            Console.WriteLine(" Press [enter] to exit.");
-            Console.ReadLine();
+        public void Run()
+        {
+            var channel = Setup();
+            PublishMessages(channel);
         }
     }
 }
